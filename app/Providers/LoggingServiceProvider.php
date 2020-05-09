611923992d9cf70d2ee510452e0d03b6060d3885
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2020-05-09 17:03:47 +0300

    Adding LoggingServiceProvider.php

---
+++ app/Providers/LoggingServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use DB;
+use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class LoggingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (in_array($this->app->environment(), ['testing', 'local'])) {
+            DB::listen(static function ($query) {
+                $logger = new Logger('DB query', [
+                    new StreamHandler(
+                        storage_path('logs/db.log')
+                    )
+                ]);
+
+                $logger->debug('SQL', [
+                    $query->sql,
+                    $query->bindings,
+                    $query->time
+                ]);
+            });
+        }
+    }
+}
