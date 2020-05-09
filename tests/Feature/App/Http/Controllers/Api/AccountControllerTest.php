b551cebeb0d8b0e0d3f4515c6dd3773bf124511c
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2020-05-09 17:10:32 +0300

    Deleting AccountUpdateRequestTest

---
+++ tests/Feature/App/Http/Controllers/Api/AccountControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Api;
+
+use App\Account;
+use App\AccountHistory;
+use App\Services\CurrencyConversionService;
+use Generator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AccountControllerTest extends TestCase
+{
+    use WithFaker;
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    /**
+     * @dataProvider accountsProvider
+     *
+     * @param int $accountNumber
+     */
+    public function test_should_fetch_account_balance(int $accountNumber): void
+    {
+        $response = $this->get(sprintf('/api/balance/%d', $accountNumber));
+        $response->assertStatus(200);
+
+        self::assertIsNumeric($response['balance']);
+    }
+    /**
+     * @dataProvider accountsProvider
+     *
+     * @param int $accountNumber
+     */
+    public function test_should_updating_balance(int $accountNumber): void
+    {
+        $currency = $this->faker->randomElement(['USD', 'RUB']);
+
+        // Current balance
+        $originalBalance = Account::whereKey($accountNumber)->value('balance');
+
+        // Updating balance
+        $params = [
+            'value' => $value = $this->faker->numberBetween(-100, 100),
+            'transaction_type' => $transactionType = $this->faker->randomElement(['debit', 'credit']),
+            'currency' => $currency,
+            'change_reason' => $changeReason = $this->faker->randomElement(['stock', 'refund']),
+        ];
+        $response = $this->put(sprintf('/api/balance/%d', $accountNumber), $params);
+        $response->assertStatus(204);
+
+        // New balance
+        $changedBalance = Account::whereKey($accountNumber)->value('balance');
+        if ($currency === 'USD') {
+            $valueUsdToRub = $this->app->get(CurrencyConversionService::class)->convertUsdToRub($value);
+            self::assertEquals($originalBalance, $changedBalance - $valueUsdToRub);
+        } else {
+            self::assertEquals($originalBalance, $changedBalance - $value);
+        }
+
+        // Last history entry
+        $lastHistoryEntry = AccountHistory::getQuery()->orderByDesc('id')->first();
+        self::assertEquals($value, $lastHistoryEntry->value);
+        self::assertEquals($transactionType, $lastHistoryEntry->transaction_type);
+        self::assertEquals($currency, $lastHistoryEntry->currency);
+        self::assertEquals($changeReason, $lastHistoryEntry->change_reason);
+    }
+
+    public function accountsProvider(): Generator
+    {
+        yield 'account number 241' => [241];
+        yield 'account number 242' => [242];
+    }
+}
---
+++ tests/Unit/App/Http/Requests/AccountUpdateRequestTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit\App\Http\Requests;
+
+use App\Http\Requests\AccountUpdateRequest;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class AccountUpdateRequestTest extends TestCase
+{
+    use WithFaker;
+
+    public function test_empty(): void
+    {
+        $request = new AccountUpdateRequest();
+        self::assertCount(4, Validator::make([], $request->rules())->errors());
+    }
+
+    public function test_invalid_parameters_values(): void
+    {
+        $params = [
+            'value' => null,
+            'transaction_type' => null,
+            'currency' => null,
+            'change_reason' => null,
+        ];
+        $request = new AccountUpdateRequest($params);
+
+        self::assertCount(4, Validator::make($params, $request->rules())->errors());
+    }
+
+    public function test_valid_parameters_values(): void
+    {
+        $params['value'] = $this->faker->numberBetween(-10000, 10000);
+        $params['transaction_type'] = $this->faker->randomElement(['debit', 'credit']);
+        $params['currency'] = $this->faker->randomElement(['USD', 'RUB']);
+        $params['change_reason'] = $this->faker->randomElement(['stock', 'refund']);
+        $request = new AccountUpdateRequest($params);
+
+        self::assertCount(0, Validator::make($params, $request->rules())->errors());
+    }
+}
